373f4bb41e3b40356091e1adf85be9689c8773ed
Zack Dean <zack.dean@gmail.com>
2021-03-22 19:37:27 -0400

    Adding test files

---
+++ otherpeopleshomework/eb/test_get_dic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while read -r LINE
+do 
+	OUT=`java hash.java $LINE`
+	touch /tmp/testfile
+	echo $LINE $OUT >> /tmp/testfile
+	if [ $OUT = "Correct" ]
+	then 
+		echo $LINE
+	fi 
+done < testfile
