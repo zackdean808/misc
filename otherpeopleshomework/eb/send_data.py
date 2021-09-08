58bfb7a90e8feba36b8cff1fe0c768b06f990703
Zack Dean <zack.dean@gmail.com>
2021-09-07 22:39:41 -0400

    It's working?

--- otherpeopleshomework/eb/send_data.py
+++ otherpeopleshomework/eb/send_data.py
@@ -11,5 +11,7 @@ nl = [i.encode('utf8') for i in l[0].split()]
 
 v = int(nl[0].decode()) + int(nl[1].decode()) / int(nl[2].decode())
 str(v)
-s.send(v.encode('utf-8'),("services.cyberprotection.agency", 9999))
+#s.send(v.encode('utf-8'),("services.cyberprotection.agency", 9999))
+#s.sendall(b'v',("services.cyberprotection.agency", 9999) )
+s.sendall(b'v')
 s.close()
