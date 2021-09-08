c32be2938a2ec57fdec35b00b5aaebc082060535
Zack Dean <zack.dean@gmail.com>
2021-09-07 23:03:01 -0400

    Clean up

--- otherpeopleshomework/eb/send_data.py
+++ otherpeopleshomework/eb/send_data.py
@@ -13,8 +13,5 @@ nl = [i.encode('utf8') for i in l[0].split()]
 v = (int(nl[0].decode()) * int(nl[1].decode())) / int(nl[2].decode())
 print ("This is v: " + str(int(v)))
 
-#s.send(v.encode('utf-8'),("services.cyberprotection.agency", 9999))
-#s.sendall(b'v',("services.cyberprotection.agency", 9999) )
-
 s.sendall(b'v')
 s.close()
