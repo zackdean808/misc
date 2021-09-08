96b606c763551f3345103ce55c70b2f31b2aa578
Zack Dean <zack.dean@gmail.com>
2021-09-08 10:10:11 -0400

    made some changes

--- otherpeopleshomework/eb/send_data.py
+++ otherpeopleshomework/eb/send_data.py
@@ -5,6 +5,7 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("services.cyberprotection.agency", 9999))
 data = s.recv(1024)
+print (data)
 l = []
 l.append(data.decode().strip())
 nl = [i.encode('utf8') for i in l[0].split()]
@@ -13,5 +14,5 @@ nl = [i.encode('utf8') for i in l[0].split()]
 v = (int(nl[0].decode()) * int(nl[1].decode())) / int(nl[2].decode())
 print ("This is v: " + str(int(v)))
 
-s.sendall(b'v')
+s.sendall(str(v).encode('utf8'))
 s.close()
